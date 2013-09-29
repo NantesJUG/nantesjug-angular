5e04f4151cf7623bcc32db928c3c19049e03181a
Manuel Boillod <manuel.boillod@zenika.com>
2013-09-29 22:27:52 +0200

    Fix jshint

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,51 +2,51 @@
 
 var places = {
   epsi: {
-      name: 'EPSI Nantes',
-      address: '2 Rue Fénelon, 44000 Nantes',
-      maps: 'http://goo.gl/maps/EO03M'
+    name: 'EPSI Nantes',
+    address: '2 Rue Fénelon, 44000 Nantes',
+    maps: 'http://goo.gl/maps/EO03M'
   },
   cantine: {
     name: 'Cantine numérique',
     address: 'cantine, 44000 Nantes',
     maps: 'http://goo.gl/maps/EO03M'
   }
-}
+};
 
 // Load from yml with https://github.com/jeremyfa/yaml.js
 var speakers = {
-  sebastien_prunier: {
-        name: 'Sebastien Prunier',
-        gravatar: 'sebastien.prunier@gmail.com',
-        photo: null,
-        bio: "\"Je suis Développeur chez SERLI, spécialisé dans les technologies Java, Cloud et NoSQL (MongoDB, Couchbase). Je m'intéresse plus récemment au langage Javascript, côté client (Angular.js) et côté serveur (Node.js) . Je fais aussi quelques contributions autour de la solution BPM Bonita. Vous pouvez me croiser dans différentes conférences : JUGs, Devoxx France, JugSummerCamp, BreizhCamp, Google Dev Fest ...\""
-      },
-  alexis_moussine_pouchkine: {
-      bio:'',
-      name: 'Alexis Moussine-Pouchkine',
-      github:'',
-      twitter:'',
-      website:'',
-      email: 'alexis.mp@gmail.com',
-      gravatar: 'alexis.mp@gmail.com',
-      image:'',
-      url:''
+  sebastienPrunier: {
+    name: 'Sebastien Prunier',
+    gravatar: 'sebastien.prunier@gmail.com',
+    photo: null,
+    bio: '"Je suis Développeur chez SERLI, spécialisé dans les technologies Java, Cloud et NoSQL (MongoDB, Couchbase). Je m\'intéresse plus récemment au langage Javascript, côté client (Angular.js) et côté serveur (Node.js) . Je fais aussi quelques contributions autour de la solution BPM Bonita. Vous pouvez me croiser dans différentes conférences : JUGs, Devoxx France, JugSummerCamp, BreizhCamp, Google Dev Fest ..."'
   },
-  manuel_boillod: {
-        name: 'Manuel Boillod',
-        gravatar: 'boillodmanuel@gmail.com',
-        photo: null,
-        bio: ""
-      },
+  alexisMoussinePouchkine: {
+    bio: '',
+    name: 'Alexis Moussine-Pouchkine',
+    github: '',
+    twitter: '',
+    website: '',
+    email: 'alexis.mp@gmail.com',
+    gravatar: 'alexis.mp@gmail.com',
+    image: '',
+    url: ''
+  },
+  manuelBoillod: {
+    name: 'Manuel Boillod',
+    gravatar: 'boillodmanuel@gmail.com',
+    photo: null,
+    bio: ''
+  },
   s1: {
-        name: 'S1',
-        bio: "BIO S1"
-      },
+    name: 'S1',
+    bio: 'BIO S1'
+  },
   s2: {
-        name: 'S2',
-        bio: "BIO S2"
-      }
-}
+    name: 'S2',
+    bio: 'BIO S2'
+  }
+};
 
 var events = [
   //event 1
@@ -59,11 +59,11 @@ var events = [
     place: places.epsi,
     subjects: [
       {
-        title: "Un petit tour de l'actualité",
+        title: 'Un petit tour de l\'actualité',
         length: '20 min',
         type: 'quickie',
-        abstract: "Sébastien Prunier nous présentera les actualités, et lance par la même occasion les quickies",
-        speakers: [speakers.sebastien_prunier]
+        abstract: 'Sébastien Prunier nous présentera les actualités, et lance par la même occasion les quickies',
+        speakers: [speakers.sebastienPrunier]
       }
     ]
   },
@@ -77,26 +77,26 @@ var events = [
     place: places.cantine,
     subjects: [
       {
-        title: "Sujet 1",
+        title: 'Sujet 1',
         length: '20 min',
         type: 'quickie',
-        abstract: "abstract 1",
+        abstract: 'abstract 1',
         speakers: [speakers.s1, speakers.s2]
       },
       {
-        title: "Sujet 2",
+        title: 'Sujet 2',
         length: '1h 30min',
         type: 'talk',
-        abstract: "abstract 1",
+        abstract: 'abstract 1',
         speakers: [speakers.s1, speakers.s2]
       }
     ]
   }
 ];
 
-function getEvent(eventId){
-  for (var i = 0; i < events.length; i++){
-    if (events[i].id === eventId){
+function getEvent(eventId) {
+  for (var i = 0; i < events.length; i++) {
+    if (events[i].id === eventId) {
       return events[0];
     }
   }
@@ -108,13 +108,13 @@ angular.module('nantesjugApp')
       var event = getEvent(1);
       $scope.event = event;
 
-      var eventDetailledView = {}
+      var eventDetailledView = {};
       eventDetailledView[event.id] = true;
       $scope.eventDetailledView = eventDetailledView;
 
-      $scope.toggleDetail = function(evendId){
+      $scope.toggleDetail = function (evendId) {
         $scope.eventDetailledView[evendId] = !$scope.eventDetailledView[evendId];
-      }
+      };
 
     })
     .controller('EventsCtrl', function ($scope) {
@@ -122,27 +122,27 @@ angular.module('nantesjugApp')
 
       //Events detailled view
       var eventDetailledView = {};
-      events.forEach(function(event){
-        eventDetailledView[event.id] = false
+      events.forEach(function (event) {
+        eventDetailledView[event.id] = false;
       });
       $scope.eventDetailledView = eventDetailledView;
 
-      $scope.toggleDetail = function(evendId){
+      $scope.toggleDetail = function (evendId) {
         $scope.eventDetailledView[evendId] = !$scope.eventDetailledView[evendId];
-      }
+      };
     })
     .controller('EventCtrl', function ($scope, $routeParams) {
-      var eventId = parseInt($routeParams.eventId);
+      var eventId = parseInt($routeParams.eventId, 10);
       var event = getEvent(eventId);
       $scope.event = event;
 
-      var eventDetailledView = {}
+      var eventDetailledView = {};
       eventDetailledView[event.id] = true;
       $scope.eventDetailledView = eventDetailledView;
 
-      $scope.toggleDetail = function(evendId){
+      $scope.toggleDetail = function (evendId) {
         $scope.eventDetailledView[evendId] = !$scope.eventDetailledView[evendId];
-      }
+      };
 
     })
     .controller('SpeakersCtrl', function ($scope) {
